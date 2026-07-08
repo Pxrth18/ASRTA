f39549b201874ae77613e54b6191805515a5f140
rautparth232@gmail.com <raut6parth232@gmail.com>
2026-07-09 00:25:50 +0530

    added Astrosat mission dossier

--- app.py
+++ app.py
@@ -1,10 +1,28 @@
 from flask import Flask, render_template
+import json
 
 app = Flask(__name__)
 
+
+def load_missions():
+    with open("data/missions.json", "r") as file:
+        return json.load(file)
+
+
 @app.route("/")
 def home():
-    return render_template("index.html")
+    missions = load_missions()
+    return render_template("index.html", missions=missions)
+
+@app.route("/mission/<int:id>")
+def mission_details(id):
+
+    missions = load_missions()
+
+    mission = next((m for m in missions if m["id"] == id), None)
+
+    return render_template("mission.html", mission=mission)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- static/js/script.js
+++ static/js/script.js
@@ -30,4 +30,45 @@ counters.forEach(counter => {
 
     updateCount();
 
-});
+    
+
+    const stars = document.getElementById("stars");
+
+      for(let i=0;i<100;i++){
+
+    const star=document.createElement("div");
+
+    star.className="star";
+
+    star.style.left=Math.random()*100+"vw";
+
+    star.style.top=Math.random()*100+"vh";
+
+    star.style.animationDelay=Math.random()*5+"s";
+
+    star.style.animationDuration=(2+Math.random()*4)+"s";
+
+    stars.appendChild(star);
+
+    }
+
+    // parallax effect.
+
+    document.addEventListener("mousemove", (e) => {
+
+    const x = (e.clientX - window.innerWidth / 2) / 80;
+    const y = (e.clientY - window.innerHeight / 2) / 80;
+
+    document.querySelectorAll(".star").forEach((star, index) => {
+
+        const speed = (index % 5 + 1) * 0.08;
+
+        star.style.transform =
+            `translate(${x * speed}px, ${y * speed}px)`;
+
+    });
+
+  });
+
+}
+);
